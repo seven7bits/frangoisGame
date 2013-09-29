02d148b0b5f15dab510f4e179b364f21d8cb217b
boo1ean <boo1ean0807@gmail.com>
2013-09-29 05:36:31 +0300

    Update binding config

---
+++ 7bits/config.js
@@ -0,0 +1,15 @@
+{
+	"method": "SevenBitsKeyPress",
+	"0": {
+		"left":   { "args": ["left"] },
+		"right":  { "args": ["right"] },
+		"up":     { "args": ["up"] },
+		"down":   { "args": ["down"] },
+		"select": { "args": ["select"] },
+		"start":  { "args": ["notImplementedKey"] },
+		"a":      { "args": ["a"] },
+		"b":      { "args": ["b"] },
+		"aa":     { "args": ["notImplementedKey"] },
+		"bb":     { "args": ["notImplementedKey"] }
+	}
+}
